f3604180435aad86c35e586f33cc798b49fde13e
Matt Glazar <matt.glazar@gmail.com>
2012-10-23 23:05:01 -0700

    Progress

--- Game2.hs
+++ Game2.hs
@@ -11,24 +11,25 @@ import Control.Monad.Cont
 import Control.Monad.Cont
 import Control.Monad.State
 import Data.IORef
+import Data.Maybe
 import System.IO.Unsafe
 import Unsafe.Coerce
 
 import qualified Data.IntMap as IntMap
 
 -- Abstract.
-newtype U r m a = U { unU :: ContT r (StateT UpdateState m) a }
+newtype Update m a = U { unU :: ContT () (StateT UpdateState m) a }
   deriving (Functor, Applicative, Monad)
 
-instance MonadTrans (U r) where
+instance MonadTrans Update where
   lift = U . lift . lift
 
-type Source m a = U a m ()
-type SourceX m a = UVar a -> U a m ()
+type SourceX m a = UVar a -> Update m ()
 
 -- Abstract.
 data UpdateState = UpdateState
   { vars :: IntMap.IntMap (forall a. a)
+  , result :: forall a. Maybe a
   }
 
 -- | Suspends the current computation until the variable is
@@ -36,7 +37,7 @@ data UpdateState = UpdateState
 suspend
   :: (Monad m)
   => UVar a
-  -> U r m ()
+  -> Update m ()
 suspend = error "suspend: TODO"
 
 -- | Executes in parallel with a new 'UVar' for
@@ -45,7 +46,7 @@ parallel
   :: (Monad m)
   => SourceX m a
   -> a
-  -> U r m (UVar a)
+  -> Update m (UVar a)
 parallel = error "parallel: TODO"
 
 -- Abstract.
@@ -59,14 +60,14 @@ nextID :: IO Int
 nextID = atomicModifyIORef curID
   $ \ u -> (succ u, u)
 
-newUVar :: (Monad m) => a -> U r m (UVar a)
+newUVar :: (Monad m) => a -> Update m (UVar a)
 newUVar x = do
   varID <- return $! unsafePerformIO nextID
   let var = UVar varID
   writeUVar var x
   return var
 
-readUVar :: (Monad m) => UVar a -> U r m a
+readUVar :: (Monad m) => UVar a -> Update m a
 readUVar (UVar varID) = do
   s <- U $ lift get
   case IntMap.lookup varID (vars s) of
@@ -75,46 +76,52 @@ readUVar (UVar varID) = do
       return $ unsafeCoerce x
     Nothing -> error "readUVar: UVar not associated with this update state"
 
-awaitUVar :: (Monad m) => UVar a -> U r m a
+awaitUVar :: (Monad m) => UVar a -> Update m a
 awaitUVar (UVar varID) = error "awaitUVar: TODO"
 
-writeUVar :: (Monad m) => UVar a -> a -> U r m ()
+writeUVar :: (Monad m) => UVar a -> a -> Update m ()
 writeUVar (UVar varID) x
   = U $ lift $ modify $ \ s -> s
     { vars = IntMap.insert varID (unsafeCoerce x) (vars s) }
 
-modifyUVar :: (Monad m) => UVar a -> (a -> a) -> U r m ()
+modifyUVar :: (Monad m) => UVar a -> (a -> a) -> Update m ()
 modifyUVar var f = do
   x <- readUVar var
   writeUVar var (f x)
 
 startUpdateT
   :: (Monad m)
-  => U r m a
+  => Update m a
   -> m (a, UpdateState)
 startUpdateT m = runUpdateT initState m
   where
     initState = UpdateState
       { vars = IntMap.empty
+      , result = Nothing
       }
 
 runUpdateT
   :: (Monad m)
   => UpdateState
-  -> U r m a
+  -> Update m a
   -> m (a, UpdateState)
-runUpdateT = undefined
+runUpdateT state m = do
+  state' <- flip execStateT state
+    $ runContT (unU m)
+    $ \ x -> modify (\ s -> s { result = Just $ unsafeCoerce x })
+
+  return (unsafeCoerce . fromJust $ result state', state')
 
 execUpdateT
   :: (Monad m)
   => UpdateState
-  -> U r m ()
+  -> Update m ()
   -> m UpdateState
 execUpdateT s m = liftM snd $ runUpdateT s m
 
 data World = World (UVar String)
 
-game :: World -> U r IO ()
+game :: World -> Update IO ()
 game world = do
   scoreVar <- parallel (playerScore world) 0
   forever $ do
@@ -126,7 +133,7 @@ playerScore world scoreVar = forever $ do
   line <- inputLine world
   modifyUVar scoreVar (+ read line)
 
-inputLine :: (Monad m) => World -> U r m String
+inputLine :: (Monad m) => World -> Update m String
 inputLine (World inputLineVar) = awaitUVar inputLineVar
 
 main :: IO ()
