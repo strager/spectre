1c5795968c1d0129506fa574ad3241eded4104b2
Matt Glazar <matt.glazar@gmail.com>
2012-10-26 21:25:30 -0700

    API and sample

--- Game4.hs
+++ Game4.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Game4 where
@@ -5,10 +6,11 @@ module Game4 where
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans
-import Control.Concurrent
+import Control.Concurrent hiding (yield)
 import Control.Concurrent.Chan
 import Control.Monad.Suspend
--- import Control.Monad.Vars
+import Control.Monad.Vars
+import Data.Maybe
 
 newtype UpdateT source event m a
   = UpdateT (SuspendT event m a)
@@ -17,10 +19,49 @@ newtype UpdateT source event m a
 awaitEvent :: (Monad m) => UpdateT s e m e
 awaitEvent = UpdateT suspend
 
-gameLoop :: UpdateT () String IO ()
-gameLoop = forever $ do
+-- Stolen from Network.CGI.Protocol in package 'cgi'.
+-- BSD license, (c) Bjorn Bringert 2006.
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
+
+data Void
+
+newtype Source a = Source (Var a)
+
+-- | Yields a value to the parent process.
+yield :: (Monad m) => s -> UpdateT s e m ()
+yield = error "yield"
+
+-- | Blocks until data is received downstream.
+block :: (Monad m) => UpdateT s e m a -> UpdateT s e m a
+block = error "block"
+
+-- | Asks for the current value of a source.
+query :: (Monad m) => Source a -> UpdateT s e m a
+query = error "query"
+
+-- | Compute in parallel.  Interleave patterns of operations
+-- in the inner monad is undefined.
+parallel
+  :: (Monad m)
+  => s                -- ^ Initial value.
+  -> UpdateT s e m a  -- ^ Parallel computation.
+  -> UpdateT s' e m (Source s)
+parallel z m = error "parallel"
+
+readCurrentNumber :: (Monad m) => UpdateT Int String m Void
+readCurrentNumber = forever $ do
   line <- awaitEvent
-  liftIO $ putStrLn $ "Received event: " ++ line
+  case maybeRead line of
+    Just number -> yield number
+    Nothing -> return ()
+
+gameLoop :: UpdateT () String IO ()
+gameLoop = do
+  curNumber <- parallel 0 readCurrentNumber
+  forever . block $ do
+    num <- query curNumber
+    liftIO $ putStrLn $ "You typed the number: " ++ show num
 
 streamForever
   :: (Monad m)
